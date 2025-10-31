2d6daf15ae6245ef3345e1df8e28c8da17fa990f
unknown <chrograph@naver.com>
2025-10-31 19:21:45 +0900

    refactor(main): 구입 금액 입력 로직 분리
    getPurchaseAmount 메서드를 분리해 main 메서드 가독성 개선

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,9 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입금액을 입력해 주세요.");
-        String purchaseAmount = Console.readLine();
-        System.out.println();
+        String purchaseAmount = getPurchaseAmount();
 
         List<Lotto> lottos = Lotto.makeLottos(getLottoQuantity(purchaseAmount));
         Lotto.printLottos(lottos);
@@ -24,6 +22,15 @@ public class Application {
         double profitRate = Ranking.getProfitRate(winningStatics, purchaseAmount) * 100;
         System.out.println("총 수익률은 "+ String.format("%.1f",profitRate) +"%입니다.");
    }
+
+    private static String getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        System.out.println();
+
+        return purchaseAmount;
+    }
+
 
     private static List<Integer> getWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
