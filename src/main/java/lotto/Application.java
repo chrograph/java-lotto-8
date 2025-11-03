249cb8b696baefb49de9a7d2505b01e074b4d95f
unknown <chrograph@naver.com>
2025-11-03 12:54:08 +0900

    refactor(exception): 예외 처리 로직을 별도 클래스로 분리
    Application 클래스에 있는 비슷한 기능을 하는 예외 처리 메서드들을 Exception 클래스에 통합

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -33,64 +33,31 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
         System.out.println();
-        checkPurchaseAmountException(purchaseAmount);
+        Exception.checkPurchaseAmount(purchaseAmount);
 
         return purchaseAmount;
-    }
-
-    private static void commonInputException(String inputValue) {
-        if (inputValue == null || inputValue.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 값을 입력하지 않았습니다.");
-        }
-        if (!inputValue.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("[ERROR] 문자가 입력되었습니다.");
-        }
-    }
-
-    private static void checkPurchaseAmountException(String purchaseAmount) {
-        if ((Integer.parseInt(purchaseAmount) / 1000) == 0) {
-            throw new IllegalArgumentException("[ERROR] 금액이 1000원 미만입니다.");
-        }
-        commonInputException(purchaseAmount);
     }
 
     private static int getBonusNumber(List<Integer> winningNumber) {
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
-        checkBonusNumberException(bonusNumber, winningNumber);
+        Exception.checkBonusNumber(bonusNumber, winningNumber);
         System.out.println();
 
         return Integer.parseInt(bonusNumber);
     }
 
-    private static void checkBonusNumberException(String bonusNumber, List<Integer> winningNumber) {
-        commonInputException(bonusNumber);
-        if (winningNumber.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호랑 중복된 입력입니다.");
-        }
-    }
-
     private static List<Integer> getWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        String InputWinningNumber = Console.readLine();
+        String inputWinningNumber = Console.readLine();
         System.out.println();
 
         List<Integer> winningNumber = new ArrayList<>();
-        for (String number : InputWinningNumber.split(",")) {
+        for (String number : inputWinningNumber.split(",")) {
             winningNumber.add(Integer.parseInt(number));
         }
-
+        Exception.checkWinningNumber(winningNumber);
         return winningNumber;
-    }
-
-    private static void checkWinningNumberException(List<Integer> winningNumber) {
-        if (winningNumber.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
-        }
-        if (winningNumber.stream().distinct().count() != 0) {
-            throw new IllegalArgumentException("[ERROR] 중복된 당첨 번호가 입력되었습니다.");
-        }
-        commonInputException(winningNumber.toString());
     }
 
     private static int getLottoQuantity(String purchaseAmount) {
---
+++ src/main/java/lotto/Exception.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.List;
+
+public class Exception {
+    public static void checkCommonInput(String inputValue) {
+        if (inputValue == null || inputValue.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 값을 입력하지 않았습니다.");
+        }
+        if (!inputValue.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("[ERROR] 문자가 입력되었습니다.");
+        }
+    }
+
+    public static void checkPurchaseAmount(String purchaseAmount) {
+        checkCommonInput(purchaseAmount);
+        if ((Integer.parseInt(purchaseAmount) / 1000) == 0) {
+            throw new IllegalArgumentException("[ERROR] 금액이 1000원 미만입니다.");
+        }
+    }
+
+    public static void checkBonusNumber(String bonusNumber, List<Integer> winningNumber) {
+        checkCommonInput(bonusNumber);
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호랑 중복된 입력입니다.");
+        }
+    }
+
+    public static void checkWinningNumber(List<Integer> winningNumber) {
+        if (winningNumber.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        if (winningNumber.stream().distinct().count() != winningNumber.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 당첨 번호가 입력되었습니다.");
+        }
+    }
+
+}
