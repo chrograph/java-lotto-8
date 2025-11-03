2e110db24aeccfa232b510fe2e07cdfc4c39aa3d
unknown <chrograph@naver.com>
2025-11-03 14:32:42 +0900

    test(exception): 구매 금액 단위 테스트 추가
    빈 문자열 입력 시 예외 발생 검증
    
    1000원 미만 값 입력 시 예외 발생 검증

--- src/test/java/lotto/ExceptionTest.java
+++ src/test/java/lotto/ExceptionTest.java
@@ -1,29 +1,37 @@
 package lotto;
 
-import lotto.Exception;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-
 class ExceptionTest {
-
     @Test
-    void checkCommonInput() {
+    @DisplayName("구매 금액이 빈 문자열이면 예외 발생")
+    void checkPurchaseAmount_noneInput() {
+        //given
+        String purchaseAmount = "";
+
+        //when&then
+        assertThatThrownBy(() -> Exception.checkPurchaseAmount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 값을 입력하지 않았습니다.");
+    }
+    @Test
+    @DisplayName("구매 금액이 1000원 미만이면 예외 발생")
+    void checkPurchaseAmount_lessAmount() {
+        //given
+        String purchaseAmount = "900";
+
+        //when&then
+        assertThatThrownBy(() -> Exception.checkPurchaseAmount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 금액이 1000원 미만입니다.");
     }
 
     @Test
-    void checkPurchaseAmount() {
-    }
-
-    @Test
-    @DisplayName("보너스 번호, 당첨 번호 중복 확인")
-    void checkBonusNumber() {
+    @DisplayName("보너스 번호가 당첨 번호와 중복이면 예외 발생")
+    void checkBonusNumber_duplicate_with_winningNumber() {
         //given
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
         String bonusNumber = "6";
@@ -35,8 +43,8 @@ class ExceptionTest {
     }
 
     @Test
-    @DisplayName("당첨 번호 6개 확인")
-    void checkWinningNumber_size() {
+    @DisplayName("당첨 번호가 6개가 아니면 예외 발생")
+    void checkWinningNumber_notSix() {
         //given
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5);
 
@@ -47,7 +55,7 @@ class ExceptionTest {
     }
 
     @Test
-    @DisplayName("당첨 번호 중복 확인")
+    @DisplayName("당첨 번호에 중복이 있으면 예외 발생")
     void checkWinningNumber_duplicate() {
         //given
         List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 5);
